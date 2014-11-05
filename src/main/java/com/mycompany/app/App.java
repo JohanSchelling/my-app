fccae23c35f42c71e3f836685991916a6b5c2092
JohanSchelling <johan.schelling@icu-it.nl>
2014-11-05 17:21:31 +0100

    e
    e

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World, it is a beautiful day!" );
-        System.out.println( "And I feel fine ....");
+        System.out.println( "And I feel fine ...." );
+        System.out.println( "Lalalalalalala" );
     }
 }
